e6eba323fe18c000ba18a3c5873dc349de966aec
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2019-03-09 10:31:18 +0200

    Add getLocation for Person

--- Person.hs
+++ Person.hs
@@ -1,6 +1,10 @@
 data Person = Person {
-                      name :: String
+                      name :: String,
+                      location :: String
                      } deriving (Show)
 
 getName :: Person -> String
 getName (Person {name=n}) = n
+
+getLocation :: Person -> String
+getLocation (Person {location=l}) = l
